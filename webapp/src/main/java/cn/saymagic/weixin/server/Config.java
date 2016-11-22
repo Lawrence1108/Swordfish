784f60c096eca6fd9a911c71a72dcc814d1e7d2d
Lawrence <924110767@qq.com>
2016-11-22 17:58:11 +0800

    Update Config.java

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "yilangxianhuangchen";
+	public static final String TOKEN = "";
 
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = "9804780c456a4a26a7186923ba506a9a";
+	public static final String APIKEY = "";
 
 }
